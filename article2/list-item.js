16ace180690eac90f003e6fbf850eb84de6a3868
BlossomMarquez <marqb058@newschool.edu>
2025-02-14 15:51:58 +0000

    recommit canvas

--- article2/list-item.js
+++ article2/list-item.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     const notes = Array.isArray(noteItem.notes) ? noteItem.notes : [noteItem.notes];
                     const numbers = Array.isArray(noteItem.numbers) ? noteItem.numbers : [noteItem.numbers];
                     const names = Array.isArray(noteItem.names) ? noteItem.names : [noteItem.names];
+                    const links = Array.isArray(noteItem.links) ? noteItem.links : [noteItem.links];
 
                     const maxLength = Math.max(notes.length, numbers.length, names.length);
 
@@ -23,8 +24,9 @@ document.addEventListener("DOMContentLoaded", function () {
                         const note = notes[i] !== undefined ? notes[i] : 'N/A';
                         const number = numbers[i] !== undefined ? numbers[i] : 'N/A';
                         const name = names[i] !== undefined ? names[i] : 'N/A';
+                        const link = links[i] !== undefined ? links[i] : 'N/A';
 
-                        listItem.innerHTML = `<a name="${name}"></a> ${number} ${note}`;
+                        listItem.innerHTML = `<a name="${name}" href="${link}">${number}</a> ${note}`;
                         notesList.appendChild(listItem);
                     }
                     infoDiv.appendChild(notesList);
